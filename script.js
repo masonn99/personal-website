4666eecd6e1721787533b96fdc3f9209c0d6d4f3
masonn99 <80999622+masonn99@users.noreply.github.com>
2025-12-31 18:41:52 -0800

    feat: Use config file for personal info

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const seeMore = document.getElementById('see-more');
     const moreItems = document.getElementById('more-items');
+    
+    const emailLink = document.getElementById('email-link');
+    const linkedinLink = document.getElementById('linkedin-link');
+
+    emailLink.href = `mailto:${personalInfo.email}`;
+    linkedinLink.href = personalInfo.linkedin;
 
     seeMore.addEventListener('click', function(event) {
         event.preventDefault();
